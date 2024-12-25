420aa388dd556959f1bc260c9b1da77c1b8cb9f3
Victor Zanda <victorzanpra@gmail.com>
2024-12-25 12:05:59 +0100

    cambios nuevos nuevos

--- src/utils/eventUtils.ts
+++
@@ -1,8 +0,0 @@
-import type { Event } from '../types/match';
-
-export function createEvent(eventData: Omit<Event, 'id'>): Event {
-  return {
-    ...eventData,
-    id: crypto.randomUUID(),
-  };
-}
